59654f253790d2500dff08b2445e78be25f7f3fd
Vishank Goel <vishankgoel11@gmail.com>
2024-09-19 01:30:13 +0530

    Implemented Logging with Custom Log Message, Current Time and IP Address of the User.

---
+++ Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace CSharpIntermediate
+{
+    public class IpAddress
+    {
+        public IPAddress[] ipAddressArray = Dns.GetHostAddresses(Dns.GetHostName());
+    }
+    public static class Logger
+    {
+        public static void WriteLog(string message)
+        {
+            string logPath = ConfigurationManager.AppSettings["logPath"];
+
+            IpAddress CurrentIpAddress = new IpAddress();
+            int IpIndex=0;
+
+            for (int i = 0; i < CurrentIpAddress.ipAddressArray.Length; i++)
+            {
+                if (CurrentIpAddress.ipAddressArray[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    IpIndex = i;
+                    //Console.WriteLine(IpIndex);
+                    break;
+                }
+            }
+            //Console.WriteLine(IpIndex);
+            //Console.Read();
+            string IPAddress = CurrentIpAddress.ipAddressArray[IpIndex].ToString();
+
+            using (StreamWriter writer = new StreamWriter(logPath, true))
+            {
+                writer.WriteLine($"[{DateTime.Now}] [{IPAddress}] : {message}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace CSharpIntermediate
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your custom Error Message :");
+            string errorMessage = Console.ReadLine();
+            Logger.WriteLog(errorMessage);
+            Console.WriteLine($"The Error Message : \"{errorMessage}\" was successfully written to the log file.");
+            Console.WriteLine("Press any key to end the program!");
+            Console.ReadKey();
         }
     }
 }
